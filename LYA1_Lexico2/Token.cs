2de6fb725d0469f9ac17af1bc1a7306f434d29fa
giezinunez2 <elgiezixd35@gmail.com>
2023-09-27 00:06:01 -0600

    LYA1_Lexico2

---
+++ LYA1_Lexico2/Token.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LYA1_Lexico2
+{
+    public class Token
+    {
+        public enum Tipos
+        {
+            Identificador,Numero,Caracter, Asignacion, FinSentencia,
+            OperadorRelacional, OperadorLogico, OperadorTermino, OperadorFactor,
+            IncrementoTermino, IncrementoFactor, OperadorComparacion, Cadena, 
+            Inicio, Fin,
+            OpRelacional
+
+        }
+        private string contenido;
+        private Tipos  clasificacion;
+        public Token()
+        {
+            contenido = "";
+            clasificacion = Tipos.Identificador;
+        }
+        public void setContenido(string contenido)
+        {
+            this.contenido = contenido;
+        }
+        public void setClasificacion(Tipos clasificacion)
+        {
+            this.clasificacion = clasificacion;
+        }
+        public string getContenido()
+        {
+            return this.contenido;
+        }
+        public Tipos getClasificacion()
+        {
+            return this.clasificacion;
+        }
+    }
+}
