00ff4ef4f9882625355d7bacd89067cdd1aeb249
giezinunez2 <elgiezixd35@gmail.com>
2023-10-13 10:07:40 -0600

    aversi

--- LYA1_Lexico3/Token.cs
+++ LYA1_Lexico3/Token.cs
@@ -10,7 +10,7 @@ namespace LYA1_Lexico3
         public enum Tipos
         {
             Identificador,Numero,Caracter,Asignacion, OpRelacional,FinSentencia, OpLogico,
-            OpTermino, InTermino ,OpFactor, InFactor, OpTernario, 
+            OpTermino, InTermino ,OpFactor, InFactor, OpTernario,  
         }
         private string contenido;
         private Tipos  clasificacion;
